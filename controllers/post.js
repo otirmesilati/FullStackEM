ebadf57ad631a37ca1fbc1ab50a2401479b9c650
Otir <otirmesi@gmail.com>
2024-12-17 21:23:09 +0200

    Assets

--- controllers/post.js
+++ controllers/post.js
@@ -4,32 +4,41 @@ const getAllPosts = (req, res)=> {
     res.send('app get post');
 };
 
-const createNewPosts = (req, res)=> { 
-    res.send(req.body)
-    console.log(req.body);
-    const post = Post({
-        message: req.body.message,
-        sender: req.body.sender
-    })
-    
-    let output;
-    (async (done)=> {
-        output = await post.save((err, newPost)=>{
-            if(err){
-                res.status(400).send({
-                    'status': 'failed',
-                    'message': err.message 
-                })
-            }
-            else{
-                res.status(200).send(newPost)
-    
-            }
-        });
-        done();
-    })
+const createNewPosts = async (req, res) => { 
+    try {
+        console.log(req.body);
 
+        const post = new Post({
+            message: req.body.message,
+            sender: req.body.sender
+        });
+
+        const newPost = await post.save();  // Wait for the save operation to complete
+        res.status(200).send(newPost); // Send the saved post back to the client
+
+    } catch (err) {
+        res.status(400).send({
+            'status': 'failed',
+            'message': err.message
+        }); // Handle any errors that occurred during the save operation
+    }
 };
+
+
+    // post.save((err, newPost)=>{
+    //     if(err){
+    //         res.status(400).send({
+    //             'status': 'failed',
+    //             'message': err.message 
+    //         })
+    //     }
+    //     else{
+    //         res.status(200).send(newPost)
+
+    //     }
+    // })
+
+
 
 module.exports = {
     getAllPosts,
