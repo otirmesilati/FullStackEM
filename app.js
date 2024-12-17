dbaa37837812089981f6769c427ec4d077520866
Otir <otirmesi@gmail.com>
2024-12-17 20:18:47 +0200

    Assets

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config();
-const post_routes = require('./routes/post_routes')
-const mongoose = require("mongoose");
 
-mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser: true});
+const mongoose = require("mongoose");
+const bodyparser = require('body-parser');
+
+mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
 db.on('error', error=>{console.error(error);});
 db.once('open', () => {console.log('Connected to MongoDB');});
 
-const bodyparser = require('body-parser');
-app.use(bodyparser.urlencoded({ extended: true, limit: '1mb' }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+
+app.use(bodyparser.urlencoded({ extended:true, limit:'1mb' }));
 app.use(bodyparser.json());
+
+const post_routes = require('./routes/post_routes')
+app.use('/post', post_routes);
 
 const port = process.env.PORT
 
-
-app.use('/post', post_routes);
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
--- controllers/post.js
+++ controllers/post.js
@@ -1,9 +1,34 @@
+const Post = require('../models/post_model');
+
 const getAllPosts = (req, res)=> { 
     res.send('app get post');
 };
 
 const createNewPosts = (req, res)=> { 
-    res.send(req.body);
+    console.log(req.body);
+    const post = Post({
+        message: req.body.message,
+        sender: req.body.sender
+    })
+    
+    let output;
+    (async (done)=> {
+        output = await post.save((err, newPost)=>{
+            if(err){
+                res.status(400).send({
+                    'status': 'failed',
+                    'message': err.message 
+                })
+            }
+            else{
+                res.status(200).send(newPost)
+    
+            }
+        });
+        done();
+    })
+    console.log(output);
+
 };
 
 module.exports = {
