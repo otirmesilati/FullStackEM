013a81c3a6c1c02e71e2cdbb7aa91025e5225d42
Otir <otirmesi@gmail.com>
2025-04-04 19:13:47 +0300

    fixing open handlers issue

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv').config();
+
+const mongoose = require("mongoose");
+const bodyparser = require('body-parser');
+
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+db.on('error', error=>{console.error(error);});
+db.once('open', () => {console.log('Connected to MongoDB');});
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+
+app.use(bodyparser.urlencoded({ extended:true, limit:'1mb' }));
+app.use(bodyparser.json());
+
+const post_routes = require('./routes/post_routes')
+app.use('/post', post_routes);
+
+module.exports = app
