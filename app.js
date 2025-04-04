5f21d7697ef4192aaa41bfa06ef765b2b5f5c252
Otir <otirmesi@gmail.com>
2025-04-04 18:59:13 +0300

    Closing mongoose connection in the before all

--- app.js
+++ app.js
@@ -25,3 +25,5 @@ const port = process.env.PORT
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+module.exports = app
--- tests/post_routes.test.js
+++ tests/post_routes.test.js
@@ -1,14 +1,19 @@
+const request = require('supertest')
+const app = require('../app')
+const mongoose = require('mongoose')
+
 beforeAll(done => {
     done()
 })
 
 afterAll(done => {
+    mongoose.connection.close()
     done()
 })
 
-describe("This is my first test", () => {
-    test("This is my first test case", () => {
-        const temp = 2
-        expect(temp).toEqual(2)
+describe("This is post API test", () => {
+    test("test post get API", async() => {
+        const response = await request(app).get('/post')
+        expect(response.statusCode).toEqual(200)
     })
 })
