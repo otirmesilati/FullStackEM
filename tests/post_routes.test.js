9d7c084c9ed326e74496fa96db257c3d911d10dc
Otir <otirmesi@gmail.com>
2025-04-04 19:48:15 +0300

    Adding the post test

--- tests/post_routes.test.js
+++ tests/post_routes.test.js
@@ -12,8 +12,28 @@ afterAll(done => {
 })
 
 describe("This is post API test", () => {
+    
+    const message = "this is my test message"
+    const sender = "test_sender"
+
     test("test post get API", async() => {
         const response = await request(app).get('/post')
         expect(response.statusCode).toEqual(200)
+    })
+
+    test("test post post API", async() => {
+        const response = await request(app).post('/post').send({
+            "message": message,
+            "sender": sender
+        })
+        expect(response.statusCode).toEqual(200)
+
+        const retMessage = response.body.message
+        const retSender = response.body.sender
+        const retId = response.body._id
+
+        expect(retMessage).toEqual(message)
+        expect(retSender).toEqual(sender)
+        expect(retId).not.toEqual(null)
     })
 })
