2d14f86b1c6500191c2e7855b4fdbb19819108af
Otir <otirmesi@gmail.com>
2025-04-04 14:24:19 +0300

    Adding the first test

--- tests/post_routes.test.js
+++ tests/post_routes.test.js
@@ -1,7 +1,14 @@
 beforeAll(done => {
-    done();
+    done()
 })
 
 afterAll(done => {
-    done();
+    done()
+})
+
+describe("This is my first test", () => {
+    test("This is my first test case", () => {
+        const temp = 2
+        expect(temp).toEqual(2)
+    })
 })
