75ba6136826f9cc5e31262dfb7b62f199121e09c
Otir <otirmesi@gmail.com>
2025-04-05 09:07:09 +0300

    adding 4th api test

--- tests/post_routes.test.js
+++ tests/post_routes.test.js
@@ -15,6 +15,7 @@ describe("This is post API test", () => {
     
     const message = "this is my test message"
     const sender = "test_sender"
+    var retId = ""
 
     test("test post get API", async() => {
         const response = await request(app).get('/post')
@@ -30,10 +31,41 @@ describe("This is post API test", () => {
 
         const retMessage = response.body.message
         const retSender = response.body.sender
-        const retId = response.body._id
+        retId = response.body._id
 
         expect(retMessage).toEqual(message)
         expect(retSender).toEqual(sender)
         expect(retId).not.toEqual(null)
     })
+
+    test("test get post by id API", async() => {
+
+        const response = await request(app).get('/post/' + retId)
+        expect(response.statusCode).toEqual(200)
+
+        const retMessage = response.body.message
+        const retSender = response.body.sender
+        const retId2 = response.body._id
+
+        expect(retMessage).toEqual(message)
+        expect(retSender).toEqual(sender)
+        expect(retId2).toEqual(retId)
+
+    })
+    
+    test("test get post by sender API", async() => {
+
+        const response = await request(app).get('/post?sender=' + sender)
+        expect(response.statusCode).toEqual(200)
+
+        const retMessage = response.body[0].message
+        const retSender = response.body[0].sender
+        const retId2 = response.body[0]._id
+
+        expect(retMessage).toEqual(message)
+        expect(retSender).toEqual(sender)
+        //expect(retId2).toEqual(retId)
+
+    })
+
 })
